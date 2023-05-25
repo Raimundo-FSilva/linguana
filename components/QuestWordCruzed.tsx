d280c4dcd42decf2f5db456ff4e1d81e6d9f84bb
Raimundo Silva <raimundo.silvabv@gmail.com>
2023-05-25 01:05:00 -0400

    teste

--- components/QuestWordCruzed.tsx
+++ components/QuestWordCruzed.tsx
@@ -158,11 +158,9 @@ export const WordCruzed = (props: {onResult: (points: number) => void}) => {
     
 
     return(
-        <h4><span className="font-semibold text-slate-700">Responda a palavra cruzada.</span></h4>
         <div className="w-full h-full flex items-center gap-7 max-sm:flex-col max-sm:py-4">
-            
-
             <div className="flex flex-wrap shrink-0 w-full max-w-[380px] bg-slate-100 p-4 rounded-md">
+                <h3>"Responda a palavra cruzada."</h3>
                 {
                     data.builder.map((word, index) => {
 
@@ -180,9 +178,10 @@ export const WordCruzed = (props: {onResult: (points: number) => void}) => {
                     })
                 }
             </div>
+            <h3>"Responda a palavra cruzada.1"</h3>
             <div className="w-full flex flex-col h-full gap-6 py-4">
                 <div>
-                    
+                    <span className="font-semibold text-slate-700">Horizontal</span>
                     <span className="font-semibold text-slate-700">Horizontal</span>
                     <ul className="flex flex-col gap-3 mt-2">
                         <li className="text-slate-500 text-xs">
